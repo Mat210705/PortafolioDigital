026586f8c1e2a4732f08c5beb3bd1963aed4773d
Matias <84361033+Mat210705@users.noreply.github.com>
2021-12-13 18:43:34 -0300

    cambios 13/12/21

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const app = Vue.createApp({
+    data() {
+        return {
+           box1: false,                       
+        }
+    },
+    created() {       
+    
+    },
+
+    methods: {
+        modalForm(e){
+            console.log(e.target);
+            this.box1 = true;            
+        },
+        cerrarModalForm(e){
+            console.log(e.target)
+            this.box1 = false;
+        }
+    }
+
+
+})
+let consol = app.mount("#app")
+
 AOS.init();
